e2f1e1ce34e8723bf56fd0a58361d66c390526f8
EbonyPeguero <43017143+EbonyPeguero@users.noreply.github.com>
2018-10-04 15:46:19 -0400

    Add files via upload

---
+++ Mean & Standard Deviation Project.R
@@ -0,0 +1,12 @@
+mean(DomVio$FAM_Fel_Assault)
+# 44.875
+ mean(DomVio$FAM_Rape)
+# 3
+ mean(DomVio$DV_Rape)
+# 7.859375
+ sd(DomVio$FAM_Fel_Assault)
+# 35.09104
+ sd(DomVio$FAM_Rape)
+# 2.850787
+ sd(DomVio$DV_Rape)
+# 6.220466
